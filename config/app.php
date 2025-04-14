c14b5c1402c817bf41be0d847a0c2ace3b5ad9c0
Liamtseva Oleksandra <lamcevasaska@gmail.com>
2025-04-14 14:44:48 +0300

    :heavy_plus_sign: Change app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://pgfk-ahymcvnccvcdggjbg.polandcentral-01.azurewebsites.net'),
 
     /*
     |--------------------------------------------------------------------------
